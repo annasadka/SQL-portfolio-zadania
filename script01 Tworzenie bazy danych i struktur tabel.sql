53e44a05f8f41cfbbb51bc3112265abb58986746
annasadka <122752057+annasadka@users.noreply.github.com>
2025-04-10 20:37:57 +0200

    Update script01 Tworzenie bazy danych i struktur tabel.sql

--- script01 Tworzenie bazy danych i struktur tabel.sql
+++ script01 Tworzenie bazy danych i struktur tabel.sql
@@ -1,15 +1,12 @@
-📝 script01.sql: Tworzenie bazy danych i struktur tabel
+## 📝 script01.sql: Tworzenie bazy danych i struktur tabel
+> *Podstawy tworzenia i definiowania struktur danych*
 
-Podstawy tworzenia i definiowania struktur danych
+- Utworzenie bazy danych `sklep_internetowy`
+- Utworzenie tabel:
+  - `klienci` (id, imie, nazwisko, email, data_rejestracji, wiek)
+  - `kategorie` (id, nazwa, opis)
+- Definiowanie ograniczeń i relacji
 
-
-Utworzenie bazy danych sklep_internetowy
-Utworzenie tabel:
-
-klienci (id, imie, nazwisko, email, data_rejestracji, wiek)
-kategorie (id, nazwa, opis)
-
-
-Definiowanie ograniczeń i relacji
+<br>
 
 CREATE DATABASE sklep_internetowy;
